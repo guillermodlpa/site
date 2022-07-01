e69d3c64d5c99181210cecacc95ceed031f8fde4
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-07-01 17:25:12 +0200

    Move theme into constants folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
-import theme from "../theme";
+import theme from "../constants/theme";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => page);
