de06fa1ef216bc43fa6a0d71578cbd6357f64ade
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-07-01 12:07:33 +0200

    Update blog thumbs to use next image

--- features/blog/BlogPostSummary.tsx
+++ features/blog/BlogPostSummary.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import Image from "next/image";
 import NextLink from "next/link";
 import MagicalDivider from "../../components/MagicalDivider";
 import { getBlogPostPath } from "../../constants/paths";
@@ -11,7 +12,8 @@ export default function BlogPostSummary({
   imageSrc,
   datePublished,
   excerpt,
-}: BlogPost) {
+  preloadImage,
+}: BlogPost & { preloadImage: boolean }) {
   return (
     <Box mb={24}>
       <Box my={8}>
@@ -27,16 +29,31 @@ export default function BlogPostSummary({
         {imageSrc && (
           <Box
             sx={{
-              height: ["50vw", undefined, "auto"],
-              width: ["100%", undefined, "25%"],
-              backgroundImage: `url(${imageSrc})`,
-              backgroundSize: ["cover", undefined, "contain"],
-              backgroundPosition: "top center",
-              backgroundRepeat: "no-repeat",
+              position: "relative",
               flexShrink: 0,
+              width: ["100%", undefined, "25%"],
             }}
-          />
+          >
+            <Image
+              src={imageSrc}
+              layout="fill"
+              objectFit="contain"
+              alt={"Blog post thumbnail"}
+              priority={preloadImage}
+            />
+          </Box>
         )}
+        {/* // <Box
+        //   sx={{
+          //     height: ["50vw", undefined, "auto"],
+          //     width: ["100%", undefined, "25%"],
+          //     backgroundImage: `url(${imageSrc})`,
+          //     backgroundSize: ["cover", undefined, "contain"],
+          //     backgroundPosition: "top center",
+          //     backgroundRepeat: "no-repeat",
+          //     flexShrink: 0,
+          //   }}
+        // /> */}
         <Box>
           <Text>
             <Text as="span" variant="secondaryText">{`${formatDate(
