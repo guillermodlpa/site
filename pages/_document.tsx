321eecea6635cc1d513e97b16bd2d4ea013216de
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-08-01 18:07:36 +0200

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,33 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 const { publicRuntimeConfig } = getConfig();
 
+function FaviconTags() {
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#af6ae2" />
+      <meta name="msapplication-TileColor" content="#af6ae2" />
+    </>
+  );
+}
+
 class MyDocument extends Document {
   render() {
     return (
@@ -15,6 +42,8 @@ class MyDocument extends Document {
             }
             rel="stylesheet"
           /> */}
+
+          <FaviconTags />
 
           <script
             async
---
+++ pages/favicon.tsx
@@ -0,0 +1,39 @@
+/**
+ * This page I use to develop the Favicon using an SVG
+ * This is temporary until developing a better favicon using image editing tools.
+ */
+export default function FaviconPage() {
+  return (
+    <div style={{ width: "500px", height: "500px" }}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+        <line x1="4" y1="6" x2="28" y2="6" stroke="#af6ae2" strokeWidth="3px" />
+        <text
+          x="16"
+          y="22.5"
+          textLength="30"
+          textAnchor="middle"
+          fontSize="18px"
+          fill="#1c2831"
+        >
+          G
+        </text>
+        <line
+          x1="4"
+          y1="26"
+          x2="28"
+          y2="26"
+          stroke="#af6ae2"
+          strokeWidth="3px"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export function getStaticProps() {
+  return {
+    // returns the default 404 page with a status code of 404 in production
+    notFound: process.env.NODE_ENV !== "development",
+    props: {},
+  };
+}
