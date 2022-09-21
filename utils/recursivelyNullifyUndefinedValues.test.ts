a73e9e86ea2fb6d7adfa9e017a75b28fa0fb5d80
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-09-21 16:16:57 -0600

    Add Jest and a unit test for function that transforms undefined values

---
+++ utils/recursivelyNullifyUndefinedValues.test.ts
@@ -0,0 +1,34 @@
+import recursivelyNullifyUndefinedValues from "./recursivelyNullifyUndefinedValues";
+
+describe("recursivelyNullifyUndefinedValues", () => {
+  it("should update nested undefined values to null", () => {
+    const date = new Date();
+    const input = {
+      name: "Daniel",
+      age: 50,
+      gender: undefined,
+      birthday: date,
+      courseCount: 0,
+      foods: [
+        { id: 1, mainIngredient: null },
+        { id: 2, mainIngredient: undefined },
+        { id: 2, mainIngredient: "fish" },
+      ],
+    };
+    expect(recursivelyNullifyUndefinedValues(input)).toEqual({
+      name: "Daniel",
+      age: 50,
+      gender: null,
+      birthday: date,
+      courseCount: 0,
+      foods: [
+        { id: 1, mainIngredient: null },
+        { id: 2, mainIngredient: null },
+        { id: 2, mainIngredient: "fish" },
+      ],
+    });
+  });
+  it("should return an empty object when passed an empty object", () => {
+    expect(recursivelyNullifyUndefinedValues({})).toEqual({});
+  });
+});
