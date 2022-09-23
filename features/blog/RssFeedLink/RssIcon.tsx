2a78a660ab693c5d89378902f546108913f607a1
Guillermo de la Puente <g.puente.allott@gmail.com>
2022-09-23 09:24:39 -0600

    Fix SVG attributes of RSS Feed not recognized by React

--- features/blog/RssFeedLink/RssIcon.tsx
+++ features/blog/RssFeedLink/RssIcon.tsx
@@ -14,9 +14,9 @@ export default function RssIcon({
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <title id={titleId}>RSS Feed</title>
       <path d="M4 11a9 9 0 0 1 9 9"></path>
