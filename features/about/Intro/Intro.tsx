4ba9f25ad80e49ae7a70c111e1379b4239adc133
Guillermo de la Puente <g.puente.allott@gmail.com>
2023-01-02 18:55:55 +0100

    Update links in about page so they look like links and not buttons

--- features/about/Intro/Intro.tsx
+++ features/about/Intro/Intro.tsx
@@ -65,16 +65,13 @@ export default function Intro() {
             gap={4}
             flexWrap="wrap"
           >
-            <NextLink passHref legacyBehavior href={PATH_CONTACT}>
-              <Button variant="outline" colorScheme="primary" as={Link}>
-                Get in touch
-              </Button>
-            </NextLink>
-
+            <Text as="span">
+              <NextLink passHref legacyBehavior href={PATH_CONTACT}>
+                <Text as={Link}>Get in touch</Text>
+              </NextLink>
+            </Text>
             <NextLink passHref legacyBehavior href={PATH_NEWSLETTER}>
-              <Button variant="outline" colorScheme="primary" as={Link}>
-                Join my Personal Newsletter
-              </Button>
+              <Text as={Link}>Join my Personal Newsletter</Text>
             </NextLink>
           </Flex>
         </Box>
