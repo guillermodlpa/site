2887fa4b6e1ba91c6b6ee64a8b740e8bf8331290
Guillermo de la Puente <g.puente.allott@gmail.com>
2023-01-26 17:26:01 +0100

    Refactor BookmarkBlock to break it down into smaller components

--- features/blogPost/NotionPageRenderer/components/BookmarkBlock.tsx
+++ features/blogPost/NotionPageRenderer/components/BookmarkBlock.tsx
@@ -10,147 +10,53 @@ import {
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
-enum Status {
-  IDLE = "idle",
-  LOADING = "loading",
-  LOADED = "loaded",
-  ERROR = "error",
-}
+type RequestStatus = "iddle" | "loading" | "success" | "error";
 
-type BookmarkData = {
+type UrlData = {
   title: string;
   description: string | null;
   favicon: string | null;
   imageSrc: string | null;
 };
 
-export default function BookmarkBlock({ url }) {
-  const [status, setStatus] = useState<Status>(Status.IDLE);
-  const [bookmarkData, setBookmarkData] = useState<null | BookmarkData>(null);
+function useUnfurlUrl(url: string) {
+  const [status, setStatus] = useState<RequestStatus>("iddle");
+  const [data, setData] = useState<null | UrlData>(null);
   useEffect(() => {
-    setStatus(Status.LOADING);
+    setStatus("loading");
     const encoded = encodeURIComponent(url);
     fetch(`/api/bookmark/${encoded}`)
       .then(async (res) => {
         if (res.ok) {
           const data = await res.json();
-          setBookmarkData(data);
-          setStatus(Status.LOADED);
+          setData(data);
+          setStatus("success");
         } else {
-          setStatus(Status.ERROR);
+          setStatus("error");
         }
       })
       .catch((error) => {
         console.error(error);
-        setStatus(Status.ERROR);
+        setStatus("error");
       });
   }, [url]);
 
-  if (status === Status.ERROR) {
-    return (
-      <Box display="flex" mb={4} backgroundColor={"callout-background"} p={3}>
-        <Text wordBreak="break-word">
-          <Link isExternal href={url}>
-            {url}
-          </Link>
-        </Text>
-      </Box>
-    );
-  }
+  return { status, data };
+}
 
-  if (status === Status.LOADED) {
-    return (
-      <LinkBox>
-        <Box
-          mb={4}
-          backgroundColor={"callout-background"}
-          p={0}
-          borderRadius="sm"
-          data-url={url}
-          overflow="hidden"
-          _hover={{ backgroundColor: "callout-background-focused" }}
-          transitionProperty="background"
-          transitionDuration="fast"
-          transitionTimingFunction="ease-in-out"
-          display="flex"
-          flexDirection="row-reverse"
-          border="1px"
-          borderColor="border"
-        >
-          {bookmarkData.imageSrc && (
-            <Box position="relative" flexGrow={1}>
-              {/* we don't use next/image because we don't know the origin domain ahead of time */}
-              <ChakraImage
-                src={bookmarkData.imageSrc}
-                objectFit="cover"
-                position="absolute"
-                height="100%"
-                width="100%"
-                alt="Website preview"
-                loading="lazy"
-                fallback={null}
-                fallbackStrategy="onError"
-              />
-            </Box>
-          )}
+function ErrorFallback({ url }: { url: string }) {
+  return (
+    <Box display="flex" mb={4} backgroundColor={"callout-background"} p={3}>
+      <Text wordBreak="break-word">
+        <Link isExternal href={url}>
+          {url}
+        </Link>
+      </Text>
+    </Box>
+  );
+}
 
-          <Flex
-            flexDirection="column"
-            gap={1}
-            p={3}
-            width={bookmarkData.imageSrc ? "66%" : "100%"}
-          >
-            {bookmarkData.title && (
-              <Text
-                whiteSpace="nowrap"
-                textOverflow="ellipsis"
-                overflow="hidden"
-                fontSize="md"
-              >
-                {bookmarkData.title}
-              </Text>
-            )}
-            {bookmarkData.description && (
-              <Text
-                variant="secondaryText"
-                fontSize="sm"
-                overflow="hidden"
-                sx={{
-                  display: "-webkit-box",
-                  WebkitLineClamp: "3",
-                  WebkitBoxOrient: "vertical",
-                }}
-              >
-                {bookmarkData.description}
-              </Text>
-            )}
-
-            <Flex gap={2} alignItems="center" overflow="hidden" width="100%">
-              {bookmarkData.favicon && (
-                <ChakraImage
-                  src={bookmarkData.favicon}
-                  width={4}
-                  height={4}
-                  objectFit="cover"
-                  alt="Website icon"
-                />
-              )}
-              <Text
-                whiteSpace="nowrap"
-                textOverflow="ellipsis"
-                overflow="hidden"
-                fontSize="md"
-              >
-                <Link isExternal href={url} as={LinkOverlay}>
-                  {url}
-                </Link>
-              </Text>
-            </Flex>
-          </Flex>
-        </Box>
-      </LinkBox>
-    );
-  }
+function LoadingSkeleton() {
   return (
     <Skeleton
       height="124px"
@@ -159,4 +65,116 @@ export default function BookmarkBlock({ url }) {
       endColor="blackAlpha.300"
     />
   );
+}
+
+function UnfurledUrlPreview({
+  url,
+  urlData,
+}: {
+  url: string;
+  urlData: UrlData;
+}) {
+  return (
+    <LinkBox>
+      <Box
+        mb={4}
+        backgroundColor={"callout-background"}
+        p={0}
+        borderRadius="sm"
+        data-url={url}
+        overflow="hidden"
+        _hover={{ backgroundColor: "callout-background-focused" }}
+        transitionProperty="background"
+        transitionDuration="fast"
+        transitionTimingFunction="ease-in-out"
+        display="flex"
+        flexDirection="row-reverse"
+        border="1px"
+        borderColor="border"
+      >
+        {urlData.imageSrc && (
+          <Box position="relative" flexGrow={1}>
+            {/* we don't use next/image because we don't know the origin domain ahead of time */}
+            <ChakraImage
+              src={urlData.imageSrc}
+              objectFit="cover"
+              position="absolute"
+              height="100%"
+              width="100%"
+              alt="Website preview"
+              loading="lazy"
+              fallback={null}
+              fallbackStrategy="onError"
+            />
+          </Box>
+        )}
+
+        <Flex
+          flexDirection="column"
+          gap={1}
+          p={3}
+          width={urlData.imageSrc ? "66%" : "100%"}
+        >
+          {urlData.title && (
+            <Text
+              whiteSpace="nowrap"
+              textOverflow="ellipsis"
+              overflow="hidden"
+              fontSize="md"
+            >
+              {urlData.title}
+            </Text>
+          )}
+          {urlData.description && (
+            <Text
+              variant="secondaryText"
+              fontSize="sm"
+              overflow="hidden"
+              sx={{
+                display: "-webkit-box",
+                WebkitLineClamp: "3",
+                WebkitBoxOrient: "vertical",
+              }}
+            >
+              {urlData.description}
+            </Text>
+          )}
+
+          <Flex gap={2} alignItems="center" overflow="hidden" width="100%">
+            {urlData.favicon && (
+              <ChakraImage
+                src={urlData.favicon}
+                width={4}
+                height={4}
+                objectFit="cover"
+                alt="Website icon"
+              />
+            )}
+            <Text
+              whiteSpace="nowrap"
+              textOverflow="ellipsis"
+              overflow="hidden"
+              fontSize="md"
+            >
+              <Link isExternal href={url} as={LinkOverlay}>
+                {url}
+              </Link>
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </LinkBox>
+  );
+}
+
+export default function BookmarkBlock({ url }: { url: string }) {
+  const { data, status } = useUnfurlUrl(url);
+
+  if (status === "error") {
+    return <ErrorFallback url={url} />;
+  }
+  if (status === "success") {
+    return <UnfurledUrlPreview url={url} urlData={data} />;
+  }
+  return <LoadingSkeleton />;
 }
