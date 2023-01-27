d8ce78fc665f6befdfb366bd58deb61e61af2c1b
Guillermo de la Puente <g.puente.allott@gmail.com>
2023-01-27 14:42:15 +0100

    Add google script to keep track of ad conversions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
+import Script from "next/script";
 import theme from "../constants/theme";
 
 function MyApp({ Component, pageProps }) {
@@ -19,6 +20,20 @@ function MyApp({ Component, pageProps }) {
 
         <meta name="robots" content="all" />
       </Head>
+
+      {/* Not happy to add Google tracking, but I'm making experiments with Google Ads and this is necessary to track conversions */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11079551803"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-11079551803');`}
+      </Script>
 
       <ChakraProvider theme={theme}>
         {getLayout(<Component {...pageProps} />)}
